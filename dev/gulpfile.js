96c01031a4578cadc7a8c3bdff5e3b6f0ac2ee5f
Nikolay <kolashuk@gmail.com>
2020-07-18 20:16:43 +0300

    created project structure

---
+++ dev/gulpfile.js
@@ -0,0 +1,41 @@
+const   gulp            = require('gulp'),
+        less            = require('gulp-less'),
+        uglify          = require('gulp-uglify-es').default,
+        cssnano         = require('gulp-cssnano'),
+        rename          = require('gulp-rename'),
+        imagemin        = require('gulp-imagemin'),
+        pngquant        = require('imagemin-pngquant'),
+        autoprefixer    = require('gulp-autoprefixer');
+
+gulp.task('less', function () {
+    return gulp.src(['./less/*.less', '!./less/variables.less'])
+        .pipe(less())
+        .pipe(autoprefixer({overrideBrowserslist: ['last 20 versions', '> 1%', 'ie 8', 'ie 7'], cascade: false }))
+        .pipe(cssnano())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('../dist/css'))
+});
+
+gulp.task('js-minify', function () {
+    return gulp.src('./js/*.js')
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('../dist/js'));
+});
+
+gulp.task('img', function () {
+    return gulp.src('./images/**/*')
+        .pipe(imagemin({
+            interlaced: true,
+            progressive: true,
+            svgPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('../dist/img'));
+});
+
+gulp.task('build', gulp.series('less', 'js-minify', 'img'));
+
+gulp.task('watch', function () {
+    gulp.watch('.', gulp.series('less', 'js-minify', 'img'));
+});
