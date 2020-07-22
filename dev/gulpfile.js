d0709f80c7caf34e8ee538d136462e4855aef1ad
kola108 <kolashuk@gmail.com>
2020-07-22 14:34:36 +0300

    gulp improvement

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -1,10 +1,10 @@
 const   gulp            = require('gulp'),
-        less            = require('gulp-less'),
-        uglify          = require('gulp-uglify-es').default,
-        cssnano         = require('gulp-cssnano'),
-        rename          = require('gulp-rename'),
-        autoprefixer    = require('gulp-autoprefixer'),
-        connect         = require('gulp-connect');
+    less            = require('gulp-less'),
+    uglify          = require('gulp-uglify-es').default,
+    cssnano         = require('gulp-cssnano'),
+    rename          = require('gulp-rename'),
+    autoprefixer    = require('gulp-autoprefixer'),
+    connect         = require('gulp-connect');
 
 gulp.task('less', function () {
     return gulp.src(['./less/*.less', '!./less/variables.less'])
@@ -24,12 +24,10 @@ gulp.task('js-minify', function () {
         .pipe(connect.reload())
 });
 
-gulp.task('webserver', function() {
-    connect.server({
-      livereload: true,
-      root: ['..', '.dist']
-    });
-  });
+gulp.task('html', function () {
+    return gulp.src('../dist/*.html')
+        .pipe(connect.reload())
+})
 
 gulp.task('build', gulp.series('less', 'js-minify'));
 
@@ -37,6 +35,8 @@ gulp.task('watch', function () {
     connect.server({
         livereload: true,
         root: '../dist'
-      });
-    gulp.watch('.', gulp.series('less', 'js-minify'));
+    });
+    gulp.watch('../dist/*.html', gulp.task('html'));
+    gulp.watch('./less/*.less', gulp.task('less'));
+    gulp.watch('./js/*.js', gulp.task('js-minify'));
 });
